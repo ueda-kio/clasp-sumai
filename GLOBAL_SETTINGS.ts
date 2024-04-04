eac97bfed203ef83fe11f03d6d184080fe873a15
k-ueda <k_ueda@waku-2.com>
2024-04-04 17:39:06 +0900

    refactor assignee.ts

--- GLOBAL_SETTINGS.ts
+++ GLOBAL_SETTINGS.ts
@@ -4,6 +4,7 @@ const GLOBAL_SETTINGS = {
   assigneeColumn: 'K', // ナレシェア担当者に★をつける列
   dateColumn: 'L', // 最後に担当した日付が入っている列
   startRow: 14, // 一人目のメンバーの行
+  assignedMark: '★', // 担当者に★をつける
   channelId: 'C04ERSPG47Q', // general
   meetingTitle: '住まいFEG会', //! MEMO: スケジュールのタイトルが変更された場合反映すること
 
--- assignee.ts
+++ assignee.ts
@@ -2,6 +2,7 @@ const activeSheet = SpreadsheetApp.getActiveSpreadsheet()!;
 
 type Data = { name: string; row: number; date: Date };
 
+/** 現在時刻を返す。GAS上でズレが生じることを避けるため分、秒、ミリ秒に 0 を設定 */
 function getDate() {
   const date = new Date();
   date.setMinutes(0);
@@ -11,33 +12,16 @@ function getDate() {
 }
 
 /**
- * - リストの中から最も古い日付の要素2つを返す
- * - 日付が同じ場合はランダムで選択
+ * ナレシェア担当者の最終担当日リストを取得
+ * - 直近のナレシェア担当者から「★」を外し現在日時を設定
+ * - 新規参画者（現在日時なしのメンバー）には現在日時を設定
  */
-function getOldTow(sortedDataList: Data[]) {
-  const uniqueDate = Array.from(new Set(sortedDataList.map((obj) => obj.date)));
-
-  const chosen: Data[] = [];
-  for (const date of uniqueDate) {
-    const sameNumObjects = sortedDataList.filter((obj) => obj.date.getTime() === date.getTime());
-    while (chosen.length < 2 && sameNumObjects.length > 0) {
-      const randomIndex = Math.floor(Math.random() * sameNumObjects.length);
-      chosen.push(sameNumObjects[randomIndex]);
-      sameNumObjects.splice(randomIndex, 1);
-    }
-    if (chosen.length === 2) break;
-  }
-
-  return chosen;
-}
-
 function getDataList() {
-  const { nameColumn, assigneeColumn, dateColumn, startRow } = GLOBAL_SETTINGS;
+  const { nameColumn, assigneeColumn, dateColumn, startRow, assignedMark } = GLOBAL_SETTINGS;
   const endRow = activeSheet.getLastRow();
   const dateList: Data[] = [];
   for (let i = startRow; i <= endRow; i++) {
-    const nameCell = activeSheet.getRange(`${nameColumn}${i}`);
-    const nameValue: string = nameCell.getValue();
+    const nameValue: string = activeSheet.getRange(`${nameColumn}${i}`).getValue();
     if (!nameValue) break; // 名前が入っていない場合は終了
 
     const assigneeCell = activeSheet.getRange(`${assigneeColumn}${i}`);
@@ -47,18 +31,18 @@ function getDataList() {
     let dateValue: Date | '' = dateCell.getValue();
 
     // '★'がある場合直近担当者のため現在日時を挿入
-    if (staredValue === '★') {
-      const date = getDate();
-      dateCell.setValue(date);
-      dateValue = date;
+    if (staredValue === assignedMark) {
+      const now = getDate();
+      dateCell.setValue(now);
+      dateValue = now;
       assigneeCell.setValue('');
     }
 
     // 日付がない場合現在日時を挿入（新規参画者想定）
     if (dateValue === '') {
-      const date = getDate();
-      dateCell.setValue(date);
-      dateValue = date;
+      const now = getDate();
+      dateCell.setValue(now);
+      dateValue = now;
     }
     dateList.push({ name: nameValue, row: i, date: dateValue });
   }
@@ -67,20 +51,39 @@ function getDataList() {
   return dateList.sort((a, b) => a.date.getTime() - b.date.getTime());
 }
 
+/** リストの中から最も古い日付の要素2つを返す。日付が同じ場合はランダムで選択 */
+function getOldTow(sortedDataList: Data[]) {
+  const uniqueDate = Array.from(new Set(sortedDataList.map((obj) => obj.date)));
+
+  const assignees: Data[] = [];
+  for (const date of uniqueDate) {
+    const sameNumObjects = sortedDataList.filter((obj) => obj.date.getTime() === date.getTime());
+    while (assignees.length < 2 && sameNumObjects.length > 0) {
+      const randomIndex = Math.floor(Math.random() * sameNumObjects.length);
+      assignees.push(sameNumObjects[randomIndex]);
+      sameNumObjects.splice(randomIndex, 1);
+    }
+    if (assignees.length === 2) break;
+  }
+
+  return assignees;
+}
+
+/** 抽選された担当者に「★」マークをつける */
 function setAssignee(assigneesList: Data[]) {
-  const { assigneeColumn } = GLOBAL_SETTINGS;
+  const { assigneeColumn, assignedMark } = GLOBAL_SETTINGS;
 
   const assignMemberNames = assigneesList.map((data) => {
-    activeSheet.getRange(`${assigneeColumn}${data.row}`).setValue('★').setFontColor('#e69138');
+    activeSheet.getRange(`${assigneeColumn}${data.row}`).setValue(assignedMark).setFontColor('#e69138');
     return data.name;
   });
 
-  return assignMemberNames;
+  // return assignMemberNames;
 }
 
 function run() {
   if (!activeSheet) throw new Error('シートが見つかりません');
   const sortedDataList = getDataList();
   const assignees = getOldTow(sortedDataList);
-  const assignMemberNames = setAssignee(assignees);
+  setAssignee(assignees);
 }
